4e71063d81b300aee3b208ac1d0717611b76497a
Blessings Emulyn <blessingsemulyn@gmail.com>
2026-05-01 02:31:31 +0200

    modified the fix file

--- fix_paths.php
+++ fix_paths.php
@@ -1,6 +1,5 @@
 <?php
-$files = array_merge(glob("*.php"), glob("admin_*.php"));
-$count = 0;
+$files = array_merge(glob("*.php"), glob("admin_*.php"), glob("includes/*.php"));
 foreach ($files as $file) {
     $content = file_get_contents($file);
     $new = preg_replace('/(require_once|include_once)\s+__DIR__\s*\.\s*[\'"]\.\.\/config\.php[\'"]/', '\1 \'config.php\'', $content);
@@ -8,8 +7,7 @@ foreach ($files as $file) {
     if ($new !== $content) {
         file_put_contents($file, $new);
         echo "Fixed: $file\n";
-        $count++;
     }
 }
-echo "Fixed $count files. Delete this script now.\n";
+echo "Done.\n";
 ?>
