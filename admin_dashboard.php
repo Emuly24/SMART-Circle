09d9f88ea47f421c50d5cc65c17c8f509d235cc4
Blessings Emulyn <blessingsemulyn@gmail.com>
2026-05-23 13:21:42 +0200

    added admin mark exercise

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -104,6 +104,7 @@ $suspensions = $conn->query("SELECT COUNT(*) FROM users WHERE status='suspended'
                 <a href="admin_note_editor.php">Write Note</a>
                 <a href="admin_notes_list.php">Manage Notes</a>
                 <a href="admin_upload_book.php">Upload Book</a>
+                <a href="admin_mark_exercises.php">Mark Exercises</a>
                 <a href="admin_book_questions.php">📚 Book Questions</a>
                 <a href="admin_bulk_import.php">📤 Bulk Import Notes/Books (CSV)</a>
                 <a href="admin_resources.php">📂 Student Resource Submissions</a>
